999a9c4f3a3ce686c7a2ebe488ee84922510833c
bsides44 <maddog.star@gmail.com>
2022-09-23 11:30:10 +1200

    debugging hit test

--- build/ar-cursor.js
+++ build/ar-cursor.js
@@ -1,20 +1,27 @@
 /* jshint esversion: 9 */
 /* global THREE, AFRAME */
 (function() {
+    console.log("start ar-cursor")
+
     "use strict";
     const direction = new THREE.Vector3();
   
     AFRAME.registerComponent("ar-cursor", {
       dependencies: ["raycaster"],
       init() {
+        console.log("init ar-cursor raycaster")
+
         const sceneEl = this.el;
         sceneEl.addEventListener("enter-vr", () => {
           if (sceneEl.is("ar-mode")) {
+        console.log("scene is in ar-mode")
+
             sceneEl.xrSession.addEventListener("select", this.onselect.bind(this));
           }
         });
       },
       onselect(e) {
+        console.log("on select cursor")
         const frame = e.frame;
         const inputSource = e.inputSource;
         const referenceSpace = this.el.renderer.xr.getReferenceSpace();
@@ -33,11 +40,16 @@
           const obj = el.object3D;
           let elVisible = obj.visible;
           obj.traverseAncestors(parent => {
+        console.log("checking intersections")
+
             if (parent.visible === false ) {
               elVisible = false
+        console.log("make invisible ")
+
             }
           });
           if (elVisible) {
+        console.log("element is visible")
             
             // Cancel the ar-hit-test behaviours
             this.el.components['ar-hit-test'].hitTest = null;
